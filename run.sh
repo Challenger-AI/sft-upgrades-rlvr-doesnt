153af4b3f11da90e2ad8ff216fd25fb2934139cc
ciao oaic <ciaooaic@ciao-MacBook-Air.local>
2025-06-27 19:27:47 +0300

    deepeek -> deepseek

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # run all python files in src folder
 
 for f in src/qwq/*.py; do python "$f"; done
-for f in src/deepeek/*.py; do python "$f"; done
+for f in src/deepseek/*.py; do python "$f"; done
 for f in src/base/*.py; do python "$f"; done
